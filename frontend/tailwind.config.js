d4470e9da8ad91077e324b886f424e9ca54af817
Victor Allen <vamuigua@gmail.com>
2024-09-01 09:10:31 +0300

    chore: cleanup unused code & setup tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+
+
+const colors = require("tailwindcss/colors");
+
+export default {
+  content: ['./src/**/*.{vue,js}'],
+  theme: {
+    extend: { colors },
+  },
+  plugins: []
+}
